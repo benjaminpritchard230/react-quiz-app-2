c217757946e9cabb4250bf5bd0b8fa938927781e
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-09-17 20:56:29 +0100

    Changes to UserAnswersDisplay

--- src/components/UserAnswersDisplay.jsx
+++ src/components/UserAnswersDisplay.jsx
@@ -4,18 +4,22 @@ const UserAnswersDisplay = ({ questions, userAnswers }) => {
     questions.map((question, index) => {
       const questionDisplay = question.questionText;
       const answerDisplay = userAnswers[index];
+      let correctAnswer = false;
+      if (question.answer === userAnswers[index]) {
+        correctAnswer = true;
+      }
       return array.push(
         <>
           <b>
-            <p>{questionDisplay}</p>
+            <p>
+              {questionDisplay}{" "}
+              {correctAnswer ? "" : `Correct answer: ${question.answer}.`}
+            </p>
           </b>
-          <p
-            style={
-              question.answer === userAnswers[index]
-                ? { color: "green" }
-                : { color: "red" }
-            }
-          >{`Your answer: ${answerDisplay}`}</p>
+          <p style={correctAnswer ? { color: "green" } : { color: "red" }}>
+            {`Your answer: ${answerDisplay}`}{" "}
+            {correctAnswer ? "\u2713" : "\u00d7"}
+          </p>
         </>
       );
     });
